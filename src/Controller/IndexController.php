904efc710b39e77fd3b557c363e385af3b163ee8
Aliou Bop <bopaliou1994@gmail.com>
2021-07-15 06:27:34 +0000

    mise en place de la page d'acceuil

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -6,15 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class IndexController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
      */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('index/index.html.twig', []);
     }
 }
